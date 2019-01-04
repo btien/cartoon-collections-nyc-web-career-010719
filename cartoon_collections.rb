54b0db3f52e74ed3a59c81c071f396c1af5b7716
Ben Tien <benjamin.tien@flatironschool.com>
2019-01-04 03:08:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.find do |num|
+  array.find do |item|
+    item.find
   cheese_types = ["cheddar", "gouda", "camembert"]
 end

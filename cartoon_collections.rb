7778608eccc3ba61eb50dc81b0a6061556ab6b36
Ben Tien <benjamin.tien@flatironschool.com>
2019-01-04 03:31:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,8 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
-  array.find do |item item.|
+  array.find do |item|
+    array.include?(cheese_types)
   end
   
 end
